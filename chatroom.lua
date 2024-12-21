08e5403bc8d136b4bd0f25d2b9ff6a7967234c8f
Hasim Rahman <hr0xqwerty@gmail.com>
2024-12-21 00:56:49 +0000

    commit

--- chatroom.lua
+++ chatroom.lua
@@ -1,1 +1,14 @@
 Members = Members or {}
+
+-- Modify `chatroom.lua` to include a handler for `Members`
+-- to register to the chatroom with the following code:
+
+Handlers.add(
+    "Register",
+    { Action = "Register"},
+    function (msg)
+      table.insert(Members, msg.From)
+      print(msg.From .. " Registered")
+      msg.reply({ Data = "Registered." })
+    end
+  )
